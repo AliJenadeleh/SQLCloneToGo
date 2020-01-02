b67a15022df98821e803d942318528de79a851eb
ali jenadeleh <alijenadeleh@outlook.com>
2020-01-02 10:47:10 -0800

    first commit

---
+++ SQLCloneToGo/frmClone.Designer.cs
@@ -0,0 +1,233 @@
+﻿namespace SQLCloneToGo
+{
+    partial class frmClone
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmClone));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtSource = new System.Windows.Forms.TextBox();
+            this.txtTarget = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.listOutput = new System.Windows.Forms.ListBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnGo = new System.Windows.Forms.Button();
+            this.txtSrcDB = new System.Windows.Forms.TextBox();
+            this.txtTargetDB = new System.Windows.Forms.TextBox();
+            this.linkMain = new System.Windows.Forms.LinkLabel();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 8);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(50, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Source : ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 34);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(44, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Target :";
+            // 
+            // txtSource
+            // 
+            this.txtSource.Location = new System.Drawing.Point(62, 5);
+            this.txtSource.Name = "txtSource";
+            this.txtSource.ReadOnly = true;
+            this.txtSource.Size = new System.Drawing.Size(524, 20);
+            this.txtSource.TabIndex = 2;
+            this.txtSource.Text = "Data Source=.\\ZOMOROD;Initial Catalog=zomorod722-1;Integrated Security=True;";
+            this.txtSource.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // txtTarget
+            // 
+            this.txtTarget.Location = new System.Drawing.Point(62, 31);
+            this.txtTarget.Name = "txtTarget";
+            this.txtTarget.ReadOnly = true;
+            this.txtTarget.Size = new System.Drawing.Size(524, 20);
+            this.txtTarget.TabIndex = 3;
+            this.txtTarget.Text = "Data Source=.\\ZOMOROD;Initial Catalog=zomorod722-1;Integrated Security=True;";
+            this.txtTarget.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(6, 57);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(45, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Output :";
+            // 
+            // listOutput
+            // 
+            this.listOutput.FormattingEnabled = true;
+            this.listOutput.Location = new System.Drawing.Point(62, 57);
+            this.listOutput.Name = "listOutput";
+            this.listOutput.Size = new System.Drawing.Size(708, 329);
+            this.listOutput.TabIndex = 2;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.btnClose);
+            this.panel1.Controls.Add(this.btnClear);
+            this.panel1.Controls.Add(this.btnGo);
+            this.panel1.Location = new System.Drawing.Point(56, 392);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(725, 78);
+            this.panel1.TabIndex = 3;
+            // 
+            // btnClose
+            // 
+            this.btnClose.Location = new System.Drawing.Point(6, 3);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(117, 72);
+            this.btnClose.TabIndex = 2;
+            this.btnClose.Text = "&Close";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(461, 3);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(117, 72);
+            this.btnClear.TabIndex = 2;
+            this.btnClear.Text = "Cle&ar";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // btnGo
+            // 
+            this.btnGo.Location = new System.Drawing.Point(597, 3);
+            this.btnGo.Name = "btnGo";
+            this.btnGo.Size = new System.Drawing.Size(117, 72);
+            this.btnGo.TabIndex = 1;
+            this.btnGo.Text = "&Go";
+            this.btnGo.UseVisualStyleBackColor = true;
+            this.btnGo.Click += new System.EventHandler(this.btnGo_Click);
+            // 
+            // txtSrcDB
+            // 
+            this.txtSrcDB.Location = new System.Drawing.Point(592, 5);
+            this.txtSrcDB.Name = "txtSrcDB";
+            this.txtSrcDB.Size = new System.Drawing.Size(178, 20);
+            this.txtSrcDB.TabIndex = 0;
+            this.txtSrcDB.Text = "4651";
+            this.txtSrcDB.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.txtSrcDB.TextChanged += new System.EventHandler(this.txtSrcDB_TextChanged);
+            // 
+            // txtTargetDB
+            // 
+            this.txtTargetDB.Location = new System.Drawing.Point(592, 31);
+            this.txtTargetDB.Name = "txtTargetDB";
+            this.txtTargetDB.Size = new System.Drawing.Size(178, 20);
+            this.txtTargetDB.TabIndex = 1;
+            this.txtTargetDB.Text = "zomorod722-1";
+            this.txtTargetDB.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.txtTargetDB.TextChanged += new System.EventHandler(this.txtTargetDB_TextChanged);
+            // 
+            // linkMain
+            // 
+            this.linkMain.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.linkMain.AutoSize = true;
+            this.linkMain.Location = new System.Drawing.Point(3, 482);
+            this.linkMain.Name = "linkMain";
+            this.linkMain.Size = new System.Drawing.Size(67, 13);
+            this.linkMain.TabIndex = 5;
+            this.linkMain.TabStop = true;
+            this.linkMain.Text = "AliJenadeleh";
+            this.linkMain.Click += new System.EventHandler(this.linkMain_Click);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Location = new System.Drawing.Point(0, 477);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(775, 22);
+            this.statusStrip1.TabIndex = 6;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // frmClone
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(775, 499);
+            this.Controls.Add(this.linkMain);
+            this.Controls.Add(this.txtTargetDB);
+            this.Controls.Add(this.txtSrcDB);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.listOutput);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtTarget);
+            this.Controls.Add(this.txtSource);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.statusStrip1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "frmClone";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "SQL Clone to Go";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.frmClone_FormClosing);
+            this.Load += new System.EventHandler(this.frmClone_Load);
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtSource;
+        private System.Windows.Forms.TextBox txtTarget;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ListBox listOutput;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnGo;
+        private System.Windows.Forms.TextBox txtSrcDB;
+        private System.Windows.Forms.TextBox txtTargetDB;
+        private System.Windows.Forms.LinkLabel linkMain;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+    }
+}
+
---
+++ SQLCloneToGo/frmClone.cs
@@ -0,0 +1,329 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SQLCloneToGo
+{
+    public partial class frmClone : Form
+    {
+        private const string AJLINK = "https://alijenadeleh.ir";
+        private const string SCRIPTALLDATA = "select * from [{0}]";
+        private const string SCRIPTCLEARTABLE = "delete from [{0}]";
+        private const string STRINGCONNECTIONTEMPLATE = @"Data Source=.\ZOMOROD;Initial Catalog={0};Integrated Security=True;";
+        private const string SCRIPTTABLELIST = @"SELECT TABLE_NAME 
+FROM [{0}].INFORMATION_SCHEMA.TABLES 
+WHERE TABLE_TYPE = 'BASE TABLE'";
+        private DataTable SourceTables;
+        
+        /// <summary>
+        /// check the inputs values
+        /// </summary>
+        /// <returns>bool</returns>
+        private bool IsValidate()
+        {
+            bool result = true;
+            string Message = "";
+
+            if (string.IsNullOrEmpty(txtSrcDB.Text))
+            {
+                Message = "Source database name required .";
+                result = false;
+            }else if (string.IsNullOrEmpty(txtTargetDB.Text))
+            {
+                Message = "Target database name required .";
+                result = false;
+            }else
+            if (txtSrcDB.Text == txtTargetDB.Text)
+            {
+                Message = "Source database name and target database name can`t be the same .";
+                result = false;
+            }
+
+            if (!result)
+                MessageBox.Show(Message, "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+
+            return result;
+        }
+
+        /// <summary>
+        /// Clear input control`s
+        /// </summary>
+        private void ClearInputs()
+        {
+            txtSrcDB.Clear();
+            txtTargetDB.Clear();
+        }
+
+        /// <summary>
+        /// Clear the output list
+        /// </summary>
+        private void ClearOutput()
+        {
+            listOutput.Items.Clear();
+        }
+
+        /// <summary>
+        /// Reload and set connection string text box's
+        /// </summary>
+        private void ReLoadConnectionString()
+        {
+            txtSource.Text = string.Format(STRINGCONNECTIONTEMPLATE, txtSrcDB.Text);
+            txtTarget.Text = string.Format(STRINGCONNECTIONTEMPLATE, txtTargetDB.Text);
+        }
+
+        /// <summary>
+        /// Check and return a SqlConnection Object
+        /// </summary>
+        /// <param name="ConnectionString">Connection String to link</param>
+        /// <returns>SqlConnection</returns>
+        private async Task<SqlConnection> GetConnectionAsync(string ConnectionString)
+        {
+            var cnn = new SqlConnection(ConnectionString);
+            try
+            {
+                await cnn.OpenAsync();
+                return cnn;
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
+        }
+
+        /// <summary>
+        /// Get All tables in a Sql Data Base
+        /// </summary>
+        /// <param name="Connection">SqlConnection Object</param>
+        /// <param name="DbName">DataBase Name</param>
+        /// <returns>string[]</returns>
+        private async Task<string[]> GetTablesAsync(SqlConnection Connection,string DbName)
+        {
+            return await Task.Run(() => { 
+                var cmd = new SqlCommand(string.Format(SCRIPTTABLELIST, DbName), Connection);
+                var dat = new SqlDataAdapter(cmd);
+                var tbl = new DataTable();
+                dat.Fill(tbl);
+                var res = new List<string>();
+                string tmp;
+                foreach (DataRow Row in tbl.Rows)
+                {
+                    tmp = Row[0].ToString();
+                    res.Add(tmp);
+                    listOutput.Invoke(new Action(() => { 
+                        listOutput.Items.Add($"{tmp} from {DbName} detected.");
+                    }));
+                }
+
+                return res.ToArray();
+            });
+        }
+
+        /// <summary>
+        /// Copy all data from Source to Target
+        /// </summary>
+        /// <param name="Tables">Table name list</param>
+        /// <param name="CnnSrc">Source Connection Object</param>
+        /// <param name="CnnTarget">Target Connection Object</param>
+        public void CopyTo(string[] Tables,SqlConnection CnnSrc,SqlConnection CnnTarget)
+        {
+            var insScript = new StringBuilder();
+            DataTable tblSrc = new DataTable();
+            SqlDataAdapter datSrc = new SqlDataAdapter();
+
+            SqlCommand cmdSrc = new SqlCommand("",CnnSrc)
+                       , cmdTarget = new SqlCommand("",CnnTarget);
+
+            foreach(string tableName in Tables)
+            {
+                listOutput.Items.Add($"{tableName} copy start.");
+                cmdSrc.CommandText = string.Format(SCRIPTALLDATA, tableName);
+                datSrc.SelectCommand = cmdSrc;
+                tblSrc = new DataTable();
+                datSrc.Fill(tblSrc);
+                try
+                {
+                    cmdTarget.CommandText = string.Format(SCRIPTCLEARTABLE, tableName);
+                    cmdTarget.ExecuteNonQuery();
+
+                    if (tblSrc.Rows.Count > 0)
+                    {
+
+                        string columns = GetColumnName(tblSrc);
+
+                        foreach (DataRow row in tblSrc.Rows)
+                        {
+                            cmdTarget.CommandText = $" insert into {tableName}({columns}) select {RowToString(row)} ";
+                            cmdTarget.ExecuteNonQuery();
+                        }
+
+                    }
+                    listOutput.Items.Add($"{tableName} copy done.");
+                }catch(Exception ex)
+                {
+                    listOutput.Items.Add($"{tableName} copy failed.");
+                    listOutput.Items.Add(ex.Message);
+                }
+            }
+
+            // check if the src is not empty
+            // clear target
+            // copy to target            
+        }
+
+        private string GetColumnName(DataTable dataTable)
+        {
+            bool test = false;
+            var res = new StringBuilder();
+
+            for(int i = 0; i < dataTable.Columns.Count; i++)
+            {
+                if (test)
+                {
+                    res.Append("," + dataTable.Columns[i].ColumnName);
+                }
+                else
+                {
+                    res.Append(dataTable.Columns[i].ColumnName);
+                    test = true;
+                }
+
+
+
+            }
+
+            return res.ToString();
+        }
+
+        /// <summary>
+        /// Convert an DataRow Object to SQL like string
+        /// </summary>
+        /// <param name="row"></param>
+        /// <returns></returns>
+        private string RowToString(DataRow row)
+        {
+            bool test = false;
+            var res = new StringBuilder();
+            string tmp;
+            for(int i = 0; i < row.ItemArray.Count(); i++)
+            {
+                if(row[i].GetType() == Type.GetType("System.String")){
+                    tmp = $"'{row[i].ToString()}'";
+                }else if(row[i].GetType() == Type.GetType("System.String")){
+                    tmp = $"'{row[i].ToString()}'";
+                }
+                else{
+                    tmp = row[i].ToString();
+                }
+                
+                if (string.IsNullOrEmpty(tmp)) tmp = "null";
+
+                    if (test)
+                        res.Append($",{tmp}");
+                    else
+                    {
+                        res.Append(tmp);
+                        test = true;
+                    }
+
+            }
+
+            return res.ToString();
+        }
+
+        public frmClone()
+        {
+            InitializeComponent();
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private async void btnGo_Click(object sender, EventArgs e)
+        {
+            (sender as Button).Enabled = false;
+            txtSrcDB.Enabled = false;
+            txtTargetDB.Enabled = false;
+            ClearOutput();
+            if (IsValidate())
+            {
+                var cnnsrc = await GetConnectionAsync(txtSource.Text);
+                var cnndest =await GetConnectionAsync(txtTarget.Text);
+
+                if(cnnsrc != null && cnndest != null)
+                {
+                    // get source tables list
+                    var tables =await GetTablesAsync(cnnsrc, txtSrcDB.Text);
+                    // Copy Data
+                    CopyTo(tables, cnnsrc, cnndest);
+                }
+                else
+                {
+                    MessageBox.Show("Connection Instance set to null");
+                }
+                
+                // foreach tbl in tables
+                // copy from source to target
+                // Make backup
+                // done
+            }
+
+            (sender as Button).Enabled = true;
+            txtSrcDB.Enabled = true;
+            txtTargetDB.Enabled = true;
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            ClearInputs();
+            ClearOutput();
+        }
+
+        private void txtSrcDB_TextChanged(object sender, EventArgs e)
+        {
+            ReLoadConnectionString();
+        }
+
+        private void txtTargetDB_TextChanged(object sender, EventArgs e)
+        {
+            ReLoadConnectionString();
+        }
+
+        private void frmClone_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            var p = new Properties.Settings();
+            try
+            {
+                p.SrcDBName = txtSrcDB.Text;
+                p.TargetDBName = txtTargetDB.Text;
+                p.Save();
+            }
+            catch { }
+        }
+
+        private void frmClone_Load(object sender, EventArgs e)
+        {
+            var p = new Properties.Settings();
+            try
+            {
+                txtSrcDB.Text = p.SrcDBName;
+                txtTargetDB.Text = p.TargetDBName;
+                ReLoadConnectionString();
+            }
+            catch { }
+        }
+
+        private void linkMain_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start(AJLINK);
+        }
+    }
+}
